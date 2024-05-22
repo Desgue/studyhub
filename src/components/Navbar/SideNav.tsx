c88cfcc65888a0fb766ad37df9d6d79d837b817c
Desgue <guedesgomero@gmail.com>
2024-05-22 12:13:51 +0100

    add modal skeleton to sidebar

--- src/components/Navbar/SideNav.tsx
+++ src/components/Navbar/SideNav.tsx
@@ -2,6 +2,14 @@ import { FaSpotify } from "react-icons/fa";
 import { FaYoutube } from "react-icons/fa";
 import { MdOutlineTimer } from "react-icons/md";
 import { IoSettingsOutline } from "react-icons/io5";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 
 function SideNav() {
   return (
@@ -10,10 +18,7 @@ function SideNav() {
         <IconContainer icon={<FaSpotify size={28} />} tooltip="Spotify" />
         <IconContainer icon={<FaYoutube size={32} />} tooltip="Youtube" />
         <IconContainer icon={<MdOutlineTimer size={30} />} tooltip="Pomodoro" />
-        <IconContainer
-          icon={<IoSettingsOutline size={30} />}
-          tooltip="Configurações"
-        />
+        <SettingsDialog />
       </div>
     </>
   );
@@ -31,10 +36,31 @@ function IconContainer({
   return (
     <div className="sidebar-icons group">
       {icon}
-      <span className="sidebar-tooltips group-hover:scale-100">
-        {" "}
-        {tooltip}{" "}
-      </span>
+      <span className="sidebar-tooltips group-hover:scale-100">{tooltip}</span>
     </div>
+  );
+}
+
+function SettingsDialog() {
+  return (
+    <>
+      <Dialog>
+        <DialogTrigger>
+          <IconContainer
+            icon={<IoSettingsOutline size={30} />}
+            tooltip="Configurações"
+          />
+        </DialogTrigger>
+        <DialogContent className="bg-gray-800 text-white border-none ">
+          <DialogHeader>
+            <DialogTitle className="text-center "> Settings </DialogTitle>
+            <DialogDescription className="text-white">
+              This action cannot be undone. This will permanently delete your
+              account and remove your data from our servers.
+            </DialogDescription>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
+    </>
   );
 }
